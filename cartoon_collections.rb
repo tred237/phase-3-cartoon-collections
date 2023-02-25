794af0b9cf53b12880c77cd53b6f0dfd672fb8ac
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-24 21:34:19 -0700

    commit tree 3d3f6995b9f61908d449ee58dfb5aa855d9f8b11

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves dwarves
+  dwarves.each_with_index{|e,i| puts "#{i+1}. #{e}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteers
+  planeteers.map{|i| "#{i.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.find{|i| i.length > 4} ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find{|i| cheese_types.find{|c| c == i}}
 end
+
+find_the_cheese(["me","you", "them"])
+find_the_cheese(["me","cheddar", "them"])
